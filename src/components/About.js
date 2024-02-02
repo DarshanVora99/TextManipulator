83715090f9cb504b1980b0fedd7fc81a0251be8b
DarshanVora99 <darshan.22110769@viit.ac.in>
2024-02-02 23:22:44 +0530

    Updated the About Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,41 @@
 import React, { useState } from "react";
 
 export default function About() {
-    let [myStyle, setMyStyle] = useState({
-        backgroundColor:"white",
-        color:"black"
-    }) 
+  let [myStyle, setMyStyle] = useState({
+    backgroundColor: "white",
+    color: "black"
+  })
 
-    let [mode, setMode] = useState("Dark Mode") 
+  let [mode, setMode] = useState("Dark Mode")
 
-    
 
-    const toggleDarkLightMode = ()=>{
-        if(mode == "White Mode"){
-            setMyStyle({
-                backgroundColor:"white",
-                color:"black",
-                
-            })
 
-            setMode("Dark Mode")
-        }
-        else{
-            setMyStyle({
-                backgroundColor:"black",
-                color:"white",
-                
-            })
+  const toggleDarkLightMode = () => {
+    if (mode == "White Mode") {
+      setMyStyle({
+        backgroundColor: "white",
+        color: "black",
 
-            setMode("White Mode")
-        }
-        
+      })
+
+      setMode("Dark Mode")
     }
+    else {
+      setMyStyle({
+        backgroundColor: "black",
+        color: "white",
+
+      })
+
+      setMode("White Mode")
+    }
+
+  }
 
   return (
 
     <div className="container my-3" style={myStyle}>
-        <h1 className="text-center">About Us</h1>
+      <h1 className="text-center">About Us</h1>
       <div className="accordion" id="accordionExample" style={myStyle}>
         <div className="accordion-item">
           <h2 className="accordion-header">
@@ -48,7 +48,7 @@ export default function About() {
               aria-expanded="true"
               aria-controls="collapseOne"
             >
-              Accordion Item #1
+              ABOUT THE PROJECT TEXT-MANUPULATOR
             </button>
           </h2>
           <div
@@ -57,55 +57,25 @@ export default function About() {
             data-bs-parent="#accordionExample"
           >
             <div className="accordion-body" style={myStyle}>
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              style={myStyle}
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-            >
-              Accordion Item #2
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            className="accordion-collapse collapse"
-            meta-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        
-          </div>
+              <strong>Created modular and reusable React components for efficient and scalable UI development
+                • Utilized JavaScript methods to enable users to easily convert input text to both lower and UPPER
+                CASE,Capitalize Case
+                • Integrated a customizable dark and light mode, allowing users to personalize their experience based on their
+                preferences</strong>
 
-            <div className="container my-4">
-          <button type="button" onClick={toggleDarkLightMode} className="btn btn-primary my-3">{mode}</button>
             </div>
+          </div>
         </div>
+
+
+      </div>
+   
+        
+
+    <div className="container my-4">
+      <button type="button" onClick={toggleDarkLightMode} className="btn btn-primary my-3">{mode}</button>
+    </div>
+        </div >
       
     
   );
